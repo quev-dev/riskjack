6f069b897d5066379ee4a1f2558eec410c645399
QuevDev <quevdev@outlook.com>
2023-04-17 14:39:00 -0400

    Added socket events to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
 import io from "socket.io-client";
-import { lazy, Suspense, useState } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Loading from "./pages/Loading";
 
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:3001", {
+  extraHeaders: {
+    user_id: sessionStorage.getItem("user_id"),
+    game_id: sessionStorage.getItem("game_id"),
+  },
+});
+
+socket.on("pass_ids", (userID, gameID) => {
+  sessionStorage.setItem("user_id", userID);
+  sessionStorage.setItem("game_id", gameID);
+});
+
 const Home = lazy(() => import("./pages/Home"));
 const Practice = lazy(() => import("./pages/Practice"));
 const FindGame = lazy(() => import("./pages/FindGame"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 export default function App() {
-  const [curPage, setCurPage] = useState("Home");
-
   return (
     <>
       <Suspense fallback={<Loading />}>
